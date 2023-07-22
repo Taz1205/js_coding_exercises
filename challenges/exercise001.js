10c68e469598a732ffeff68f1443196989aa8871
Fatima Zahra <fathima_z@yahoo.com>
2023-07-23 00:17:34 +0100

    refactored exercise001 challenges

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -5,30 +5,28 @@
 // Note: Be sure to read the corresponding .md file for each exercise, in the docs folder. 📘 👍
 
 export function capitalize(word) {
-  if (word === undefined) throw new Error("A word is required");
-  else if (typeof word !== "string" || word.length === 0) {
+  //if (word === undefined) throw new Error("A word is required");
+   if (typeof word !== "string" || word.length === 0) {
     console.log("A word is required");
   }
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
 export function generateInitials(firstName, lastName) {
-  if (firstName === undefined) throw new Error("firstName is required");
-  if (lastName === undefined) throw new Error("lastName is required");
+  //if (firstName === undefined) throw new Error("firstName is required");
+  //if (lastName === undefined) throw new Error("lastName is required");
   if (typeof firstName !== "string" || firstName.length === 0) {
     console.log("first name is required");
   }
   if (typeof lastName !== "string" || lastName.length === 0) {
     console.log("second name is required");
   }
-  const firstInitial = firstName.charAt(0).toUpperCase();
-  const lastInitial = lastName.charAt(0).toUpperCase();
-  return firstInitial + "." + lastInitial;
+  return `${firstName[0]}.${lastName[0]}`;
 }
 
 export function addVAT(originalPrice, vatRate) {
-  if (originalPrice === undefined) throw new Error("originalPrice is requied");
-  if (vatRate === undefined) throw new Error("vatRate is required");
+  //if (originalPrice === undefined) throw new Error("originalPrice is requied");
+  //if (vatRate === undefined) throw new Error("vatRate is required");
   if (
     typeof originalPrice !== "number" ||
     typeof vatRate !== "number" ||
@@ -38,32 +36,22 @@ export function addVAT(originalPrice, vatRate) {
     console.log("Invalid number");
     return NaN;
   }
-  const vatAmount = originalPrice * (vatRate / 100);
+ /* const vatAmount = originalPrice * (vatRate / 100);
   const totalPrice = originalPrice + vatAmount;
   if (!Number.isInteger(totalPrice)) {
     return Number(totalPrice.toFixed(2));
-  }
-  return totalPrice;
+  }  
+   return totalPrice;*/
+  return Number((originalPrice + (vatRate / 100) * originalPrice).toFixed(2));
+
 }
 
 export function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  if (originalPrice <= 50 && reduction === 0) {
-    reduction = 0;
-  } else if (originalPrice < 80 && reduction === 15) {
-    reduction = 0.15;
-  } else if (originalPrice <= 100 && reduction === 33.3) {
-    reduction = 0.333;
-  } else if (originalPrice >= 100 && reduction === 50) {
-    reduction = 0.5;
-  }
-  const salePrice = originalPrice - originalPrice * reduction;
-  if (Number.isInteger(salePrice)) {
-    return salePrice;
-  } else {
-    return Number(salePrice.toFixed(2));
-  }
+  return Number(
+		(originalPrice - (reduction / 100) * originalPrice).toFixed(2)
+	);
 }
 
 export function getMiddleCharacter(str) {
@@ -72,70 +60,53 @@ export function getMiddleCharacter(str) {
     console.log("str is required");
   }
   // Calculate the middle index and length
-  const middleIndex = Math.floor(str.length / 2);
-  const middleLength = str.length % 2 === 0 ? 2 : 1;
-  if (middleLength === 1) {
-    return str.slice(middleIndex, middleIndex + middleLength);
-  } else {
-    let middleIndex1 = middleIndex - 1;
-    return str.slice(middleIndex1, middleIndex1 + middleLength);
-  }
+  return str.length % 2 === 0
+		? str[str.length / 2 - 1] + str[str.length / 2]
+		: str[Math.floor(str.length / 2)];
 }
-
 export function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
   if (typeof word !== "string" || word === "") {
     console.log("word is required");
     return "word is required";
   }
-  const characters = word.split("");
-  const reversedCharacters = characters.reverse();
-  const reversedString = reversedCharacters.join("");
-  return reversedString;
+  return word.split('').reverse().join('');
 }
 
 export function reverseAllWords(words) {
-  if (words === undefined) throw new Error("words are required");
+  //if (words === undefined) throw new Error("words are required");
   if (!Array.isArray(words)) {
     console.log("words are required");
     return "words are required";
   }
-  const reversedWords = words.map((word) => reverseString(word));
-  return reversedWords;
-}
-function reverseString(str) {
-  return str.split("").reverse().join("");
+  return words.map((word) => reverseWord(word));
+   
 }
 
+
 export function countLinuxUsers(users) {
-  if (users === undefined) throw new Error("users are required");
+  //if (users === undefined) throw new Error("users are required");
   if (!Array.isArray(users) || users.length === 0) {
     return "users are required";
   }
-  let linuxUserCount = 0;
-  for (let i = 0; i < users.length; i++) {
-    const user = users[i];
-    if (user.type === "Linux") {
-      linuxUserCount++;
-    }
-  }
-  return linuxUserCount;
+  return users.filter((user) => user.type === 'Linux').length;
 }
 
 export function getMeanScore(scores) {
-  if (scores === undefined) throw new Error("scores are required");
+  //if (scores === undefined) throw new Error("scores are required");
   if (!Array.isArray(scores) || scores.length === 0) {
     console.log("scores are required");
     return "scores are required";
   }
-  const sum = scores.reduce((acc, score) => acc + score, 0);
-  const mean = sum / scores.length;
-  if (Number.isInteger(mean)) return mean;
-  else return Number(mean.toFixed(2));
+  const mean = scores.reduce(
+		(total, score) => total + score / scores.length,
+		0
+	);
+	return Math.round(mean * 100) / 100;
 }
 
 export function simpleFizzBuzz(n) {
-  if (n === undefined) throw new Error("n is required");
+  //if (n === undefined) throw new Error("n is required");
   if (typeof n !== "number" || isNaN(n)) {
     return "Invalid input";
   }
---
+++ test/test/exercise005.test.js
@@ -0,0 +1,238 @@
+import {
+  findNextNumber,
+  count1sand0s,
+  reverseNumber,
+  sumArrays,
+  arrShift,
+  findNeedle,
+  getWordFrequencies,
+} from "../challenges/exercise005";
+
+describe("findNextNumber", () => {
+  test("returns the next number after the given number in the array", () => {
+    expect(findNextNumber([5, 3, 7, 8, 1, 10], 7)).toBe(8);
+    expect(findNextNumber([5, 3, 7, 8, 1, 10], 1)).toBe(10);
+    expect(findNextNumber([4, 22, 654, 123, 65, 23, 40, 1], 22)).toBe(654);
+  });
+
+  test("if the number is not found in the array, returns null", () => {
+    expect(findNextNumber([5, 3, 7, 8, 1, 10], 55)).toBe(null);
+  });
+
+  test("if the number is found more than once, returns the number after the first instance", () => {
+    expect(findNextNumber([5, 3, 7, 8, 1, 3, 10], 3)).toBe(7);
+  });
+
+  test("if the number is found in the final index position of the array, returns null", () => {
+    expect(findNextNumber([5, 3, 7, 8, 1, 3, 10], 10)).toBe(null);
+  });
+  test("If no input, returns null", () => {
+    expect(findNextNumber([], 0)).toBe(null);
+  });
+});
+
+describe("count1sand0s", () => {
+  test("If no input, returns null", () => {
+    expect(count1sand0s("")).toBe(null);
+  });
+  test("If invalid string, returns null", () => {
+    expect(count1sand0s("23456")).toBe(null);
+  });
+  test("If input is not a string, returns null", () => {
+    expect(count1sand0s(12345)).toBe(null);
+  });
+  test("returns an object with the count of 1s and 0s in a string", () => {
+    expect(count1sand0s("11000")).toEqual({
+      1 : 2,
+      0 : 3,
+    });
+
+    expect(count1sand0s("0101010111")).toEqual({
+      1 : 6,
+      0 : 4,
+    });
+
+    expect(count1sand0s("1111111")).toEqual({
+      1 : 7,
+      0 : 0,
+    });
+
+    expect(count1sand0s("0111")).toEqual({
+      1 : 3,
+      0 : 1,
+    });
+  });
+});
+
+describe("reverseNumber", () => {
+  test("If no input, returns null", () => {
+    expect(reverseNumber()).toBe(null);
+  });
+  test("If number is not an integer, returns null", () => {
+    expect(reverseNumber(12.34)).toBe(null);
+  });
+  test("If input is not a number, returns null", () => {
+    expect(reverseNumber("34")).toBe(null);
+  });
+  test("reverses the digits of a positive or negative integer", () => {
+    expect(reverseNumber(0)).toBe(0);
+    expect(reverseNumber(5)).toBe(5);
+    expect(reverseNumber(104)).toBe(401);
+    expect(reverseNumber(12345)).toBe(54321);
+    expect(reverseNumber(100)).toBe(1); // No leading 0 necessary
+    expect(reverseNumber(-1000)).toBe(-1);
+  });
+});
+
+describe("sumArrays", () => {
+  test("If no input, returns null", () => {
+    const arrs = [];
+    expect(sumArrays(arrs)).toBe(null);
+  });
+  test("If arrs not an array, returns null", () => {
+    const arrs = "2";
+    expect(sumArrays(arrs)).toBe(null);
+  });
+  test("returns the total of the numbers in all sub arrays", () => {
+    const arrs = [[1, 2, 3], [6, 3, 1], [1], [9, 10], [3, 5]];
+    expect(sumArrays(arrs)).toBe(44);
+  });
+  test("returns the total of all sub arrays even with all zeroes", () => {
+    const arrs = [[0], [0], [0]];
+    expect(sumArrays(arrs)).toBe(0);
+  });
+});
+
+describe("arrShift", () => {
+  test("returns it as it is if not an array", () => {
+    expect(arrShift("2")).toEqual("2");
+  });
+  test("returns an array with the first and last items swapped", () => {
+    expect(arrShift([1, 2])).toEqual([2, 1]);
+    expect(arrShift([1, 2, 3])).toEqual([3, 2, 1]);
+    expect(arrShift([1, 2, 3, 4])).toEqual([4, 2, 3, 1]);
+  });
+
+  test("makes no difference when the array length is < 2", () => {
+    expect(arrShift([1])).toEqual([1]);
+    expect(arrShift([])).toEqual([]);
+    expect(arrShift([0])).toEqual([0]);
+  });
+});
+
+describe("findNeedle", () => {
+  test("returns true if any of the properties of an object contain the specified string", () => {
+    const obj1 = {
+      name: "LINNMON",
+      description: "Small round table",
+      price: 31.89,
+      store: "Warrington",
+      code: 12872,
+    };
+    expect(findNeedle(obj1, "table")).toBe(true);
+
+    // Note that the objects provided to the function could have any keys/values
+    const obj2 = {
+      product_name: "Sparkle n Shine Dishwasher Tablets",
+      price: 1.99,
+      location: "Hulme",
+      discounted: false,
+      available: true,
+    };
+    expect(findNeedle(obj2, "Dishwasher")).toBe(true);
+  });
+
+  test("returns false if none of the properties of an object contain the specified string", () => {
+    const obj1 = {
+      name: "LINNMON",
+      description: "Small round table",
+      price: 31.89,
+      store: "Warrington",
+      code: 12872,
+    };
+    expect(findNeedle(obj1, "chair")).toBe(false);
+
+    // Note that the objects provided to the function could have any keys/values
+    const obj2 = {
+      product_name: "Sparkle n Shine Dishwasher Tablets",
+      price: 1.99,
+      location: "Hulme",
+      discounted: false,
+      available: true,
+    };
+    expect(findNeedle(obj2, "Carpet Cleaner")).toBe(false);
+  });
+
+  test("The search string should not be case sensitive", () => {
+    const obj1 = {
+      name: "LINNMON",
+      description: "Small round table",
+      price: 31.89,
+      store: "Warrington",
+      code: 12872,
+    };
+
+    expect(findNeedle(obj1, "warrington")).toBe(true);
+    expect(findNeedle(obj1, "linnmon")).toBe(true);
+    expect(findNeedle(obj1, "Liverpool")).toBe(false);
+  });
+  test("returns false if empty string", () => {
+    const obj1 = {};
+
+    expect(findNeedle(obj1, "")).toBe(false);
+    expect(findNeedle(obj1, "hello")).toBe(false);
+  });
+  test("returns false if invalid input", () => {
+    const obj1 = 2;
+
+    expect(findNeedle(obj1, "")).toBe(false);
+    expect(findNeedle(obj1, "hello")).toBe(false);
+  });
+});
+
+describe("getWordFrequencies", () => {
+  test("returns false if empty string", () => {
+    expect(getWordFrequencies("")).toBe(false);
+  });
+  test("returns false if invalid string", () => {
+    expect(getWordFrequencies(123)).toBe(false);
+  });
+  test("returns the frequencies of each word in a string", () => {
+    expect(getWordFrequencies("hello world")).toEqual({
+      hello: 1,
+      world: 1,
+    });
+
+    expect(getWordFrequencies("the cat is hairier than the rat")).toEqual({
+      the: 2,
+      cat: 1,
+      is: 1,
+      hairier: 1,
+      than: 1,
+      rat: 1,
+    });
+
+    expect(getWordFrequencies("hello hello hello")).toEqual({
+      hello: 3,
+    });
+  });
+
+  test("ignores capitalisation", () => {
+    expect(getWordFrequencies("Hello hello hello")).toEqual({
+      hello: 3,
+    });
+  });
+
+  test("ignores punctuation", () => {
+    // Hint: Google "JavaScript remove special characters from string" to get some ideas!
+    expect(
+      getWordFrequencies("Hello, hello hello! What have we here?")
+    ).toEqual({
+      hello: 3,
+      what: 1,
+      have: 1,
+      we: 1,
+      here: 1,
+    });
+  });
+});
